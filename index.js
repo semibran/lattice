60b27bf3325418d4b9683728e784ae03a9686300
Brandon Semilla <semibran@gmail.com>
2017-02-23 19:15:57 -0800

    Match docs

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 module.exports = Grid
-Grid.inside = inside
 Grid.get = get
 Grid.set = set
+Grid.inside = inside
 
 function Grid(cols, rows, data) {
-	if (!data)
-		data = Array(cols * rows)
-	return { cols: cols, rows: rows, data: data }
-}
-
-function inside(grid, x, y) {
-	return x >= 0 && y >= 0 && x < grid.cols && y < grid.rows
+	return { cols: cols, rows: rows, data: data || Array(cols * rows) }
 }
 
 function get(grid, x, y) {
-	if (!inside(grid, x, y))
-		return null
-	return grid.data[y * grid.cols + x]
+	var cols = grid.cols
+	var data = grid.data
+	var inside = Grid.inside(grid)
+	return function at(x, y) {
+		if (!inside(x, y))
+			return null
+		return data[y * cols + x]
+	}
 }
 
-function set(grid, x, y, value) {
-	if (!inside(grid, x, y))
-		return null
-	return grid.data[y * grid.cols + x] = value
+function set(grid) {
+	var cols = grid.cols
+	var data = grid.data
+	var inside = Grid.inside(grid)
+	return function at(x, y) {
+		return function to(value) {
+			if (inside(x, y))
+				data[y * cols + x] = value
+			return at
+		}
+	}
+}
+
+function inside(grid) {
+	var cols = grid.cols
+	var rows = grid.rows
+	return function at(x, y) {
+		return x >= 0 && y >= 0 && x < cols && y < rows
+	}
 }
