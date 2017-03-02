15967bf588eba67fb0569e17b8c4d8e7dd15f290
Brandon Semilla <semibran@gmail.com>
2017-03-01 20:05:26 -0800

    Remove currying for simplicity

--- README.md
+++ README.md
@@ -15,28 +15,24 @@ const Grid = require('grid')
 ```javascript
 var grid = Grid(cols, rows[, data])
 ```
-
 Creates a new `Grid` instance with the given data and dimensions.
 
 ### `get`
 ```javascript
-Grid.get(grid)(x, y)
+Grid.get(grid, x, y)
 ```
-
 Gets the value at `(x, y)` on `grid`.
 
 ### `set`
 ```javascript
-Grid.set(grid)(x, y)(value)
+Grid.set(grid, x, y, value)
 ```
-
 Sets the `value` at `(x, y)` on `grid`.
 
 ### `inside`
 ```javascript
-Grid.inside(grid)(x, y)
+Grid.inside(grid, x, y)
 ```
-
 Determines if the given `(x, y)` pair is inside `grid`.
 
 ## License
--- index.js
+++ index.js
@@ -1,40 +1,20 @@
 module.exports = Grid
-Grid.get = get
-Grid.set = set
-Grid.inside = inside
 
 function Grid(cols, rows, data) {
 	return { cols: cols, rows: rows, data: data || Array(cols * rows) }
 }
 
-function get(grid, x, y) {
-	var cols = grid.cols
-	var data = grid.data
-	var inside = Grid.inside(grid)
-	return function at(x, y) {
-		if (!inside(x, y))
-			return null
-		return data[y * cols + x]
-	}
+Grid.get = function (grid, x, y) {
+	if (!Grid.inside(grid, x, y))
+		return null
+	return grid.data[y * grid.cols + x]
 }
 
-function set(grid) {
-	var cols = grid.cols
-	var data = grid.data
-	var inside = Grid.inside(grid)
-	return function at(x, y) {
-		return function to(value) {
-			if (inside(x, y))
-				data[y * cols + x] = value
-			return at
-		}
-	}
+Grid.set = function (grid, x, y, value) {
+	if (Grid.inside(grid, x, y))
+		grid.data[y * grid.cols + x] = value
 }
 
-function inside(grid) {
-	var cols = grid.cols
-	var rows = grid.rows
-	return function at(x, y) {
-		return x >= 0 && y >= 0 && x < cols && y < rows
-	}
+Grid.inside = function (grid, x, y) {
+	return x >= 0 && y >= 0 && x < grid.cols && y < grid.rows
 }
